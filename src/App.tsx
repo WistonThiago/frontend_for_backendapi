b01a33b536d2b96f267d96558e179d3351908170
WistonThiago <102038474+WistonThiago@users.noreply.github.com>
2025-06-06 18:09:09 -0300

    Creating data visualization from API.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react';
 import { FiTrash } from 'react-icons/fi';
+import { api } from './services/api';
+
+interface CustomerProps {
+  id: string;
+  name: string;
+  email: string;
+  status: boolean;
+  created_at: string;
+}
 
 export default function App() {
+
+  const [customers, setCustomers] = useState<CustomerProps[]>([]);
+
+  useEffect(() => {
+    loadCustomers();
+  }, [])
+
+  async function loadCustomers() {
+    const response = await api.get("/customers");
+    setCustomers(response.data);
+  }
   return (
     <div>
       <h1 className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
@@ -17,18 +38,22 @@ export default function App() {
             <input 
               type="submit" 
               value="Register" 
-              className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium" />
+              className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium" 
+            />
           </form>
 
-          <section className="flex flex-col">
-            <article className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
-              <p><span className="font-medium">Name: </span>Wiston</p>
-              <p><span className="font-medium">E-mail: </span>wiston@email.com</p>
-              <p><span className="font-medium">Status: </span>Active</p>
+          <section className="flex flex-col gap-4">
+            { customers.map((customer) => (
+              <article key={customer.id} className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200">
+              <p><span className="font-medium">Name: </span>{customer.name}</p>
+              <p><span className="font-medium">E-mail: </span>{customer.email}</p>
+              <p><span className="font-medium">Status: </span>{customer.status ? "Active" : "Inactive"}</p>
               <button className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2">
                 <FiTrash size={18} color="#FFF" />
               </button>
             </article>
+            ))}
+
           </section>
 
         </main>
